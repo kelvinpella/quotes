90d274bb8cda3f1323cc1b5ae212d4770ed39904
Kelvin <kelvinpella@gmail.com>
2021-08-02 07:36:29 +0300

    create quotes app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./App.module.css";
+
+const Quote = (props) => {
+  return (
+    <>
+      <p>{props.quote}</p>
+      <p>{props.author}</p>
+    </>
+  );
+};
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://type.fit/api/quotes")
+      .then((response) => {
+        setQuotes(response.data.slice(0, 2));
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  let quote = "";
+  if (loading) {
+    quote = "Loading quotes...";
+  }
+  if (quotes.length) {
+    quote = quotes.map((quote, index) => {
+      return <Quote key={index} quote={quote.text} author={quote.author} />;
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={styles.Quote_box}>
+        {quote}
+        <div>Twitter</div>
+        <div>Tumblir</div>
+      </div>
+      <p>by hezag</p>
+    </>
   );
 }
 
