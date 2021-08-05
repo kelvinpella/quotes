906e995c78d8fadd5e78a7f3bb643177d9556b1a
Kelvin <kelvinpella@gmail.com>
2021-08-05 20:19:28 +0300

    Random color generated

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ import Quote from "./components/Quote/Quote";
 
 class App extends Component {
   state = { newQuote: [], quotes: [], loading: true };
-
-  componentDidMount = () => {
-    axios
+  componentDidMount = async () => {
+    await axios
       .get("https://type.fit/api/quotes")
       .then((response) => {
-        this.setState({ quotes: response.data, loading: false }, () => {
-          this.newQuoteHandler();
-        });
+        this.setState({ quotes: response.data, loading: false }, () => {});
       })
       .catch((error) => {
         console.log(error);
       });
+    this.newQuoteHandler();
+    this.modifyQuote();
+  };
+
+  randomColorGenerator = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   };
   modifyQuote = () => {
-    // change opacity from 0 to 1 when displaying new quote
+    // get random color
+    let color = this.randomColorGenerator();
+    if (color === "#FFFFFF") {
+      color = "#342224";
+    }
+    //modify root element initial properties
+    document.querySelector("#root").style.backgroundColor = color;
+    // change opacity from 0 to 1, and color  when displaying new quote
     let parent = document.querySelector("#root").firstChild;
     for (let i = 0; i < 2; i++) {
+      parent.children[i].style.cssText = `color:${color}`;
       parent.children[i].animate([{ opacity: 1 }, { opacity: 0 }], {
         duration: 2000,
         direction: "reverse",
@@ -33,10 +49,9 @@ class App extends Component {
     }
     this.newQuoteHandler();
   };
+
   newQuoteHandler = () => {
     // create random number to 100
-    console.log("clicked");
-
     let randomIndex = Math.floor(Math.random() * 100 + 1);
     let singleQuote = this.state.quotes.filter((quote, index) => {
       return index === randomIndex;
@@ -50,7 +65,7 @@ class App extends Component {
 
   render() {
     const { newQuote, loading } = this.state;
-    // change size of parent container for the app
+    // change size of parent container(#root) for the app
     document.querySelector("#root").setAttribute("class", `${styles.Root}`);
 
     return (
